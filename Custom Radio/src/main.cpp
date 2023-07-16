65280bc2f73fd638c4e9ab2d1d27e4c6b917f050
Juan Cruz Pisacane <jcpisacane@hotmail.com>
2023-07-16 05:56:50 -0300

    Ultima version de codigo: Estado a probar

--- Custom Radio/src/main.cpp
+++ Custom Radio/src/main.cpp
@@ -1,36 +1,34 @@
 #include <Arduino.h>
 #include <ESP32Time.h>
 #include <LiquidCrystal_I2C.h>
+#include <string.h>
 
 
 int perilla_superior = 19;
 int circuito_electrico = 17; // combinacion de jacks.
-int adc_1 = 4;
+int adc_1 = 4; // combinacion de frecuencias de fichas.
 int adc_2 = 2;
 int boton_final = 18;
 
-TaskHandle_t task_reloj_handle = NULL;
-TaskHandle_t task_quest_handle = NULL;
-
 int lcdColumns = 20;
 int lcdRows = 4;
-int bateria = 20;
-
-//bool quest_COMPLETE = false;
-//bool quest_FAILED   = false;
+int bateria = 20; // tiempo total de escape en minutos... se ve reflejado en la pantalla como porcentaje de bateria.
+String mensaje_escape = "Ya hemos enviado un avion de rescate, esperar fuera del refugio con la bengala";
 
 ESP32Time rtc(3600); // init rtc
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); // init lcd
 
 bool bateria_restante(void);
-int quest();
-void quest_FAILED();
-void quest_COMPLETE();
-void parpadear_backlight();
+void init_gpio(void);
+int quest(void);
+void quest_FAILED(void);
+void quest_COMPLETE(void);
+void parpadear_backlight(void);
+void scrollText(int row, String message, int delayTime, int lcdColumns);
 
 void setup() {
   Serial.begin(115200);
-  //init_gpio();
+  init_gpio();
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,2); lcd.print("Alerta: Bateria Baja"); // leyendas iniciales en las primeras 2 filas.
@@ -49,42 +47,64 @@ void loop() {
       quest_FAILED();
       break;
   }
-  if(quest_FAILED == true){
-    
-
-  }
 }
 
 int quest(){
-  lcd.setCursor(0,0); lcd.print("---Iniciar Sistema--");
-  for(;digitalRead(perilla_superior) == HIGH;){
+  lcd.setCursor(0,0); lcd.print("                    ");
+  lcd.setCursor(0,1); lcd.print("                    ");
+  lcd.setCursor(0,0); lcd.print("   Iniciar Sistema  ");
+  for(;digitalRead(perilla_superior) == HIGH;){ // cambiar low a high.
     if (bateria_restante() == true){
     return 2; // se agoto el tiempo/ bateria.
     }
-  }
-  parpadear_backlight();
-  lcd.setCursor(0,0); lcd.print("Configurar Circuito"); lcd.setCursor(0,1); lcd.print("      Electrico     ");
-  for(;digitalRead(circuito_electrico) == HIGH;){
+  }parpadear_backlight();
+
+  lcd.setCursor(0,0); lcd.print("                    ");
+  lcd.setCursor(0,1); lcd.print("                    ");
+  lcd.setCursor(0,0); lcd.print("Configurar  Circuito"); 
+  lcd.setCursor(0,1); lcd.print("      Eléctrico     ");
+
+  for(;digitalRead(circuito_electrico) == HIGH;){ // cambiar low a high.
     if (bateria_restante() == true){
     return 2; // se agoto el tiempo/ bateria.
     }
-  }
+  }parpadear_backlight();
 
+  lcd.setCursor(0,0); lcd.print("                    ");
+  lcd.setCursor(0,1); lcd.print("                    ");
+  lcd.setCursor(0,0); lcd.print("Indicar  Coordenadas"); 
+  for(; 1900 <= analogRead(adc_1) <= 2350  &&  150 <= analogRead(adc_2) <= 520;){
+    if (bateria_restante() == true){
+    return 2; // se agoto el tiempo/ bateria.
+    }
+  }parpadear_backlight();
 
-
-
-
+  lcd.setCursor(0,0); lcd.print("                    ");
+  lcd.setCursor(0,1); lcd.print("                    ");
+  lcd.setCursor(0,0); lcd.print("  Enviar Señal SOS  ");
+  for(;digitalRead(boton_final) == HIGH;){
+    if (bateria_restante() == true){
+    return 2; // se agoto el tiempo/ bateria.
+    }
+  }parpadear_backlight();
   return 1; // se completo el escape exitosamente.
-
-  return 2; // se agoto el tiempo/ bateria.
 }
-void quest_FAILED(){
-
+void quest_FAILED(void){
+  parpadear_backlight();
+  parpadear_backlight();
+  for(;;){
+    lcd.noBacklight();
+    delay(10000);
+  }
 }
-void quest_COMPLETE(){
-
+void quest_COMPLETE(void){
+  lcd.clear(); lcd.setCursor(0,0); 
+  lcd.print("   Muy Bien Equipo! ");
+  for(;;){
+    scrollText(1, mensaje_escape, 500, 20);
+  }
 }
-void parpadear_backlight(){
+void parpadear_backlight(void){
   lcd.noBacklight();
   delay(100);
   lcd.backlight();
@@ -95,61 +115,31 @@ void parpadear_backlight(){
   delay(100);
 }
 void init_gpio(void){
-  /*pinMode(,);
-  pinMode(,);
-  pinMode(,);
-  pinMode(,);
-  pinMode(,);
-  pinMode(,);
-  */
+  pinMode(perilla_superior, INPUT);
+  pinMode(circuito_electrico,INPUT);
+  //pinMode(adc_1, INPUT);
+  //pinMode(adc_2,INPUT);
+  pinMode(boton_final,INPUT);
 }
-
-bool bateria_restante(void){
+bool bateria_restante(void){ // funciona 9, quizas se pierde un minuto por los delays.
   if(rtc.getSecond() == 00 ){ // modifica el porcentaje de bateria cada 1 min.
     bateria--;
-    lcd.setCursor(17,1);
+    lcd.setCursor(17,3);
     lcd.print(bateria);
     if(bateria == 0){
       return true;
     }
+    delay(1000); // evita que pase los minutos de una
   } return false;
 }
-
-/*
-void task_reloj(void * parameters){
-  rtc.setTime(01, 00, 00, 16, 7, 2023);  // 16th Junio 2023 00:00:01
-  int bateria = 20; // Variable que almacena el estado de la bateria y se utiliza para imprimir en pantalla.
-  lcd.print(bateria);
-  for(;;){
-    vTaskDelay(100 / portTICK_PERIOD_MS); // DELAY DE 100ms en tasks.
-    if(rtc.getSecond() == 00 ){ // modifica el porcentaje de bateria cada 1 min.
-      bateria--;
-      lcd.setCursor(17,1);
-      lcd.print(bateria);
-      if (bateria == 00){ // quizas sea necesario remover el task_... =! NULL.
-       // poner que sucede cuando se acaba la bateria. Mover a otra parte del codigo en caso de ser necesario.
-      // pone algo en la pantalla o lo que quieras
-      // leyenda opcional; "LA RADIO NO TIENE SUFICIENTE ENERGIA PARA TRANSMITIR" o algo asi.
-      Serial.println("Se acabo la bateria, escape finalizado.");
-      //lcd.clear();
-      //lcd.setCursor();
-      //lcd.print();
-      quest_COMPLETE = true;
-      vTaskDelete(task_quest_handle); // Elimina la tarea quest del procesamiento, deja de funcionar.
-      }
-    }
+void scrollText(int row, String message, int delayTime, int lcdColumns) {
+  for (int i=0; i < lcdColumns; i++) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+  for (int pos = 0; pos < message.length(); pos++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(pos, pos + lcdColumns));
+    delay(delayTime);
   }
 }
-void task_quest(void * parameters){
-  // PONE ACA TU CODIGO DEL ESCAPE.
-  /*
-  Paso 1: Accionar Perilla Superior.
-  Paso 2: Configurar Circuito electrico(Jacks 6.5mm).
-  Paso 3: Indicar Coordenadas de Indicacion ( ADC con Potenciometros, 27 y 68).
-  Paso 4: Presionar Boton.
-  
-  for(;digitalWrite() == HIGH;){
-  }
-  for(;;)
-  
-}*/
